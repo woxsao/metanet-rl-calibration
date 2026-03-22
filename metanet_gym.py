e82e7498b7fed73599c8320a7126ee44e3ad0bda
mochan <mochan@mit.edu>
2026-03-21 20:18:27 -0400

    removing 100 weight

--- metanet_gym.py
+++ metanet_gym.py
@@ -363,7 +363,7 @@ class METANETGymEnv(gym.Env):
             )
             / (self.v_max + 1e-6)
         )
-        reward = -(rho_mape + q_mape + 100 * v_mape)
+        reward = -(rho_mape + q_mape + v_mape)
         if terminated:
             rho_scale_all = np.maximum(self.rho_hat, 0.1 * self.rho_max)
             q_scale_all = np.maximum(self.q_hat, 0.1 * self.flow_max)
